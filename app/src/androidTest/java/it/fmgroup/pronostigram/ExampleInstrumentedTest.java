a16591b28a61ed45c217faeb8c6b51ae9f998d23
Luigi <luigimartire94@gmail.com>
2018-01-10 16:48:15 +0100

    prova-junit-test

--- app/src/androidTest/java/it/fmgroup/pronostigram/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/it/fmgroup/pronostigram/ExampleInstrumentedTest.java
@@ -1,9 +1,13 @@
 package it.fmgroup.pronostigram;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.widget.TextView;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,11 +20,32 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
         assertEquals("it.fmgroup.pronostigram", appContext.getPackageName());
+        //provaTest1();
     }
+
+    @Test
+    public void provaTest() {
+        Activity activity = mActivityRule.getActivity();
+        TextView tv = activity.findViewById(R.id.textview);
+        assertEquals("dovrebbero essere uguali", tv.getText().toString(), "Benvenuto!");
+        activity.finish();
+    }
+/*
+    @Test
+    public void provaTest1() {
+        Activity activity = mActivityRule.getActivity();
+        TextView tv = activity.findViewById(R.id.textview);
+        assertEquals("test che fallisce", tv.getText().toString(), "sbagliato");
+        activity.finish();
+    }
+*/
 }
