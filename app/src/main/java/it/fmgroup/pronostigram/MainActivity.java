253b8296b3136b38769dced5caafadd2a099d9e7
Antonio <antoniofarina1702>
2018-01-11 15:17:52 +0100

    initial layout

--- app/src/main/java/it/fmgroup/pronostigram/MainActivity.java
+++ app/src/main/java/it/fmgroup/pronostigram/MainActivity.java
@@ -10,8 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tv = (TextView) findViewById(R.id.textview);
-        tv.setText("Benvenuto!");
+
     }
 
 }
