91159533102258f7269bbe43df4e27b97c8cc7f5
Antonio <antoniofarina1702>
2018-01-13 17:59:16 +0100

    completato layout Login e Registrazione

--- app/src/main/java/it/fmgroup/pronostigram/MainActivity.java
+++ app/src/main/java/it/fmgroup/pronostigram/MainActivity.java
@@ -1,15 +1,38 @@
 package it.fmgroup.pronostigram;
 
+import android.net.Uri;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements InitialFragment.OnFragmentInteractionListener {
+
+    Button button_entra;
+
+    InitialFragment fragment1 = new InitialFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Begin the transaction
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        // Replace the contents of the container with the new fragment
+        ft.replace(R.id.framelayout_initial, fragment1);
+        // or ft.add(R.id.your_placeholder, new FooFragment());
+        // Complete the changes added above
+        ft.commit();
+
+
+    }
+
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
     }
 
